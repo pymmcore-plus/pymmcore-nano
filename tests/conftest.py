51bb2e94c82583a5e3e0ac68d410705a35fd91b7
Talley Lambert <talley.lambert@gmail.com>
2025-03-19 15:48:48 -0400

    fix for windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
+import atexit
 import os
 from pathlib import Path
 import sys
 from tempfile import TemporaryDirectory
+import tempfile
 from typing import Iterable
 import pytest
 import pymmcore_nano as pmn
@@ -14,24 +16,23 @@ BUILT_ADAPTERS = BUILDDIR / "src" / "mmcoreAndDevices" / "DeviceAdapters"
 @pytest.fixture(scope="session")
 def adapter_paths() -> Iterable[list[str]]:
     lib_ext = {"linux": "so", "darwin": "dylib", "win32": "dll"}[sys.platform]
+    adapter_path = Path(__file__).parent / "adapters" / sys.platform
     # find all built libraries in the builddir
     if BUILT_ADAPTERS.is_dir() and (
         libs := [x for x in BUILT_ADAPTERS.rglob(f"*.{lib_ext}")]
     ):
-        # copy them to a new temporary directory
-        with TemporaryDirectory() as tmpdir:
-            tmp_path = Path(tmpdir)
-            for lib in libs:
-                # removing extension (using stem) is important,
-                # it affects the name of the device library in micromanager.
-                lib_name = lib.name if os.name == "nt" else lib.stem
-                shutil.copy2(lib, tmp_path / lib_name)
-            yield [str(tmpdir)]
-    else:
-        adapters = Path(__file__).parent / "adapters" / sys.platform
-        if not adapters.is_dir():
-            pytest.skip(f"No adapters for {sys.platform}")
-        yield [str(adapters)]
+        adapter_path.mkdir(exist_ok=True, parents=True)
+        # NOTE: on windows this WILL leave .dll files in the adapter_path
+        # after the test is over, since they are locked by the python process.
+        atexit.register(lambda: shutil.rmtree(adapter_path, ignore_errors=True))
+        for lib in libs:
+            # removing extension (using stem) is important,
+            # it affects the name of the device library in micromanager.
+            lib_name = lib.name if os.name == "nt" else lib.stem
+            shutil.copy2(lib, adapter_path / lib_name)
+    elif not adapter_path.is_dir():
+        pytest.skip(f"No adapters for {sys.platform}")
+    yield [str(adapter_path)]
 
 
 @pytest.fixture
