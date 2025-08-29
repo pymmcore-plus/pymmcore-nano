72ee6eb9a434aaeee9ce12b029d888cc177537ae
Talley Lambert <talley.lambert@gmail.com>
2025-08-29 13:16:49 -0400

    fix sdist

--- scripts/generate_dist.py
+++ scripts/generate_dist.py
@@ -6,14 +6,6 @@ import shutil
 def main():
     """Generate distribution files for mmCoreAndDevices."""
     dist_dir = pathlib.Path(os.environ["MESON_DIST_ROOT"]).resolve()
-
-    for f in (dist_dir / "src" / "mmCoreAndDevices").iterdir():
-        if f.name not in ("MMCore", "MMDevice"):
-            if f.is_dir():
-                shutil.rmtree(f)
-            else:
-                os.remove(f)
-
     shutil.rmtree(dist_dir / "tests")
 
 
